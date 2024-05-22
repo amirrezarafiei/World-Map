0698229e991cbb894208e15f1457b6503eceadc5
amirrezarafiei <80318178+amirrezarafiei@users.noreply.github.com>
2024-05-22 15:04:15 +0330

    Add files via upload

---
+++ world map prevalence disease.R
@@ -0,0 +1,11 @@
+install.packages("rworldmap")
+library(rworldmap)
+library(sp)
+
+
+data_map <- data.frame(country = c("USA", "Canada", "Iran", "South Korea", "Brazil", "Argentina", "China", "Japan", "Iraq", "Saudi Arabia", "UK", "Germany", "Austria", "Russia", "Sweden", "Finland", "France", "India", "Norway", "Ukraine", "Pakistan", "Turkey", "Australia", "Afghanistan", "Turkmenistan", "Uzbekistan"), prevalence = c(25, 35, 77, 41, 45, 52, 40, 32, 89, 68, 32, 35, 30, 45, 27, 22, 36, 55, 26, 37, 80, 48, 30, 91, 86, 79))
+
+mapData <- joinCountryData2Map(data_map, joinCode = "NAME", nameJoinColumn = "country")
+
+mapCountryData(mapData, nameColumnToPlot = "prevalence", catMethod = "pretty", colourPalette = "negpos8", mapTitle = "Prevalence of the disease in the world")
+
